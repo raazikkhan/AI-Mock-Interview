f827d4f9306e7be6424dd0bfd6d3569cb646d29e
Razik <razik@Raziks-MacBook-Air.local>
2025-08-26 08:54:04 +0530

    database done

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -2,11 +2,25 @@
 import { api } from "@/convex/_generated/api";
 import { useUser } from "@clerk/nextjs";
 import { useMutation } from "convex/react";
+import { createContext, useContext } from "react";
 
-import React, { useEffect } from "react";
+interface UserDetails {
+  email: string;
+  imageUrl: string;
+  name: string;
+}
+
+export const UserDetailContext = createContext<{
+  userDetails: UserDetails | null;
+  setUserDetails: React.Dispatch<React.SetStateAction<UserDetails | null>>;
+} | null>(null);
+
+import React, { useEffect, useState } from "react";
+
 const Provider = ({ children }: { children: React.ReactNode }) => {
   // This component is used to create a user in the database when the user signs up.
   const { user } = useUser();
+  const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
   useEffect(() => {
     CreateNewUser();
   }, [user]);
@@ -19,10 +33,19 @@ const Provider = ({ children }: { children: React.ReactNode }) => {
         imageUrl: user.imageUrl ?? "",
         name: user.fullName ?? "",
       });
-      console.log(result);
+      console.log("user Created " + result);
+      setUserDetails(result);
     }
   };
 
-  return <div>{children}</div>;
+  return (
+    <UserDetailContext.Provider value={{ userDetails, setUserDetails }}>
+      <div>{children}</div>
+    </UserDetailContext.Provider>
+  );
 };
 export default Provider;
+
+export const useUserDetailContext = () => {
+  return useContext(UserDetailContext);
+};
